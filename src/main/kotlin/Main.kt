f2aa687077258ea4b271ff104e15796c7b0bb9ab
hamza <aheita3@gmail.com>
2023-06-18 11:59:40 +0300

    Data types and null safety

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main() {
+    testNullSafety()
+}
+
+fun testNullSafety() {
+    ////Null Safety
+    var n1: Int? = 5
+    /*
+    if x = anyValue then print this value
+    if x = null then print null
+    */
+    println(n1?.dec())
+
+
+////Not Null Assertion (not recommended)
+    var n2: Int? = 5
+    /*
+    I'm sure -> n2 not equal null
+    */
+    println(n2!!.dec())
+
+
+    //Example
+    var i: Int? = 5
+    //var f: Float? = i?.toFloat()
+    val f: Float = i?.toFloat() ?: 1f   //  (?: if) Called Elvis operator
+    /*
+    if x = null then print 1f else convert to float
+    */
+    println(f)
+}
