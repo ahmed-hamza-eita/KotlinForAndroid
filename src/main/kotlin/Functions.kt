78154525c766c2c9e19448ef04d34d12a4c75073
hamza <aheita3@gmail.com>
2023-06-19 22:14:02 +0300

    Functions

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,27 @@
+fun main() {
+    // permitAccess(19).println()
+    //permitAccess(19, 18, 20, 22, 16).println()
+
+    val list = listOf("Java", "Android", "Kotlin")
+    concatList(list, " ! ").println()
+    concatList(list).println() //Default argument (separator -> :)
+    concatList(separator = " ! ", list = list).println() //Named Argument
+}
+
+fun permitAccess(age: Int) = age >= 18
+
+
+//Function with parameter which takes collection of values using (vararg)
+fun permitAccess(vararg ages: Int): Boolean {
+    for (i in ages) {
+        if (i < 18)
+            return false
+    }
+    return true
+}
+
+//Named argument and Default argument
+fun concatList(list: List<String>, separator: String = ":") =
+    list.joinToString(separator)
+
+
