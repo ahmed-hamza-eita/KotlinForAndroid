8b50befa22249f098cb43f58869955585171051f
hamza <aheita3@gmail.com>
2023-06-19 22:48:42 +0300

    Functions

--- src/main/kotlin/Challenges.kt
+++ src/main/kotlin/Challenges.kt
@@ -7,6 +7,19 @@ fun main() {
 
 
 fun challengeLab1() {
+
+    /*
+    ■Challenge: Working with Nullables and If-Expression
+Use the readline() function to read an input from the
+command line.
+Notice the return type of readline(), and use what you
+learned to work with it.
+The user should input their name.
+If the user enters an empty string, store a default value.
+Use an if expression to define a different greeting message
+based on whether the user entered a name.
+     */
+
     "Challenge 1".printColored(31)
     "Enter your Name :".print()
     val input = readln()
@@ -21,6 +34,21 @@ fun challengeLab1() {
 
 
 fun challengeLab2() {
+    /*
+    ■Challenge2: Kotlin Collections and Loops
+Create a collection of integers.
+▲Use java.util.Random to fill the collection with 100
+random numbers between 1 and 100.
+Go through the collection from start to end and print
+its elements up to the point where an element is less
+than or equal to 10 (Do it without using "if" or "when").
+■ Hints:
+Make sure you import java.util.Random (same syntax as Java).
+If you can't elements to your collection, think about the type of
+collection you are using.
+To avoid "if", you may need to use a different type of loop.
+    */
+
     "Challenge 2".printColored(31)
     val r = Random()
     val array = IntArray(100)
