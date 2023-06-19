901c731edb3155b95d7df4513bceb3725a7b0136
hamza <aheita3@gmail.com>
2023-06-19 16:37:03 +0300

    Loops and Challenges

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,12 +1,9 @@
 fun Any?.println() {
     println(this)
 }
-<<<<<<< HEAD
-fun Any?.print() {
+ fun Any?.print() {
     print(this)
 }
-=======
->>>>>>> origin/master
 
 fun String.printColored(color: Int) {
     println("\u001B[${color}m$this\u001B[0m")  //31 red color
